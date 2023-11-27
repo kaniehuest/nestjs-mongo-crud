17e9af4221a4889c738a02d0274b2e9ff2ab1f5e
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2023-11-26 22:17:33 -0300

    Added CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/demo?authSource=admin',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
